6ff944e0853949bbe17f7dc946d68d07add6ad13
jelfalan <jesse.elfalan@tetratech>
2015-10-02 15:28:28 -0700

    localstorage for locations, with object array

--- MobileAppDemo/MobileAppDemo/Scripts/WellLocationViewModel.js
+++ MobileAppDemo/MobileAppDemo/Scripts/WellLocationViewModel.js
@@ -10,29 +10,50 @@
     self.Timestamp = timestamp;
 
 }
+    
+    //function locationVM() {
+    //    var self = this;
 
-    function locationVM() {
-        var self = this;
-
-        self.allLocations = ko.observableArray([]);
-        self.getLocations = function () {
+    //    self.allLocations = ko.observableArray([]);
+    //    self.getLocations = function () {
        
-            self.allLocations.removeAll();
+    //        self.allLocations.removeAll();
         
-            $.getJSON("/api/LocationApi/", function (data) {
-                $.each(data, function (key, val) {
-                    self.allLocations.push(new well_location(val.ID, val.LocationID, val.Dry, val.Collected, val.Skipped,                   val.Comment, val.Timestamp));
-                });
-            });
+    //        $.getJSON("/api/LocationApi/", function (data) {
+    //            $.each(data, function (key, val) {
+    //                self.allLocations.push(new well_location(val.ID, val.LocationID, val.Dry, val.Collected, val.Skipped,                   val.Comment, val.Timestamp));
+    //            });
+    //        });
+    //    }
+    //}
+
+//uses data from localstorage
+function locationVM() {
+    var self = this;
+
+    self.allLocations = ko.observableArray([]);
+    self.getLocations = function () {
+
+        self.allLocations.removeAll();
+        var retWrapper = JSON.parse(store.get("Locations"));
+        var objArray = retWrapper.items;
+        console.log(objArray);
+      
+        for (var i = 0; i < objArray.length; i++) {
+            var obj = objArray[i];
+            self.allLocations.push(new well_location(obj.ID, obj.LocationID, obj.Dry, obj.Collected, obj.Skipped, obj.Comment, obj.Timestamp));
         }
+
     }
+}
+    
 
     $(document).ready(function () {
         ko.applyBindings(new locationVM, document.getElementById('displayNode'));
       //  ko.applyBindings(new well_location, document.getElementById('createNode'));
-
-        $('#btnGetLocations').click();//refresh on page load
-
+        $(window).load(function(){
+            $('#btnGetLocations').click();//refresh on page load
+        });
         //$('#locationsList')
 
     });
---
+++ MobileAppDemo/MobileAppDemo/Scripts/init_locations.js
@@ -0,0 +1,74 @@
+﻿$(document).ready(function () {
+
+   // var store = new Lawnchair({ name: 'Locations' }, function (store) { });
+    
+    //initLocations = function (Locations){
+
+    //  //  var Locations = loc;
+    //    var key = "";
+       
+    //    for (var i = 0; i < Locations.length; i++) {
+          
+    //        key = "loc_" + Locations[i].ID;
+            
+    //        var newLocation = {
+    //            key: key,
+    //            type: 'Location',
+    //            ID : Locations[i].ID,
+    //            LocationID : Locations[i].LocationID,
+    //            Dry : Locations[i].Dry,
+    //            Collected : Locations[i].Collected,
+    //            Skipped : Locations[i].Skipped,
+    //            Comment : Locations[i].Comment,
+    //            timestamp : Locations[i].timestamp,
+    //            geostamp_lat : Locations[i].geostamp_lat,
+    //            geostamp_lon : Locations[i].geostamp_lon
+    //        };
+            
+    //        console.log("Saving newlocation with key: " + newLocation.key);
+
+    //        //lawnchair_store.save(newLocation);
+    //        store.set(newLocation.key,newLocation);
+
+    //    };
+    //}
+
+    function initLocations() {
+        console.log("initializing locations...");
+        $.getJSON("/api/LocationApi/", function (data) {
+
+            var wrapper  = { 'items': [] }; //need a key 'items' for the array or else store returns undefined
+
+            for (var i = 0; i < data.length; i++) {
+                var obj = {};
+                $.each(data[i], function (key, value) { //add each property
+                    obj[key] = value;
+                });
+                console.log("new obj: " + JSON.stringify(obj));
+                wrapper.items.push(obj);  //push to item array
+            };
+       
+           store.set("Locations", JSON.stringify(wrapper));
+        });
+
+       // var retWrapper = JSON.parse(store.get("Locations"));
+        //var retWrapper = JSON.parse(localStorage.getItem("Locations"));
+          //  console.log(retWrapper);
+
+    };
+
+    function checkStore(str, callBack) {
+        if (store.get(str) == null) {
+            callBack();
+        }
+        else {
+            console.log(str + " already initialized"); //debug
+        }
+    };
+   
+    checkStore("Locations", initLocations);
+
+
+
+});
+
