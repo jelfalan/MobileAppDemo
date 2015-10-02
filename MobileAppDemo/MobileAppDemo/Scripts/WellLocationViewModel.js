57a578dde53ec6621a2a3f1199af875fa3009cf5
jelfalan <jesse.elfalan@tetratech>
2015-10-02 16:15:21 -0700

    object hashmap for storage

--- MobileAppDemo/MobileAppDemo/Scripts/WellLocationViewModel.js
+++ MobileAppDemo/MobileAppDemo/Scripts/WellLocationViewModel.js
@@ -10,22 +10,22 @@
     self.Timestamp = timestamp;
 
 }
-    
-    //function locationVM() {
-    //    var self = this;
 
-    //    self.allLocations = ko.observableArray([]);
-    //    self.getLocations = function () {
-       
-    //        self.allLocations.removeAll();
-        
-    //        $.getJSON("/api/LocationApi/", function (data) {
-    //            $.each(data, function (key, val) {
-    //                self.allLocations.push(new well_location(val.ID, val.LocationID, val.Dry, val.Collected, val.Skipped,                   val.Comment, val.Timestamp));
-    //            });
-    //        });
-    //    }
-    //}
+//function locationVM() {
+//    var self = this;
+
+//    self.allLocations = ko.observableArray([]);
+//    self.getLocations = function () {
+
+//        self.allLocations.removeAll();
+
+//        $.getJSON("/api/LocationApi/", function (data) {
+//            $.each(data, function (key, val) {
+//                self.allLocations.push(new well_location(val.ID, val.LocationID, val.Dry, val.Collected, val.Skipped,                   val.Comment, val.Timestamp));
+//            });
+//        });
+//    }
+//}
 
 //uses data from localstorage
 function locationVM() {
@@ -36,24 +36,23 @@ function locationVM() {
 
         self.allLocations.removeAll();
         var retWrapper = JSON.parse(store.get("Locations"));
-        var objArray = retWrapper.items;
-        console.log(objArray);
-      
-        for (var i = 0; i < objArray.length; i++) {
-            var obj = objArray[i];
-            self.allLocations.push(new well_location(obj.ID, obj.LocationID, obj.Dry, obj.Collected, obj.Skipped, obj.Comment, obj.Timestamp));
-        }
+        var objMap = retWrapper.map;
+        console.log(objMap);
+
+        $.each(objMap, function (key, value) {
+            self.allLocations.push(new well_location(value.ID, value.LocationID, value.Dry, value.Collected, value.Skipped, value.Comment, value.Timestamp));
+        });
 
     }
 }
-    
 
-    $(document).ready(function () {
-        ko.applyBindings(new locationVM, document.getElementById('displayNode'));
-      //  ko.applyBindings(new well_location, document.getElementById('createNode'));
-        $(window).load(function(){
-            $('#btnGetLocations').click();//refresh on page load
-        });
-        //$('#locationsList')
 
+$(document).ready(function () {
+    ko.applyBindings(new locationVM, document.getElementById('displayNode'));
+    //  ko.applyBindings(new well_location, document.getElementById('createNode'));
+    $(window).load(function () {
+        $('#btnGetLocations').click();//refresh on page load
     });
+    //$('#locationsList')
+
+});
--- MobileAppDemo/MobileAppDemo/Scripts/init_locations.js
+++ MobileAppDemo/MobileAppDemo/Scripts/init_locations.js
@@ -1,16 +1,16 @@
 ﻿$(document).ready(function () {
 
-   // var store = new Lawnchair({ name: 'Locations' }, function (store) { });
-    
+    // var store = new Lawnchair({ name: 'Locations' }, function (store) { });
+
     //initLocations = function (Locations){
 
     //  //  var Locations = loc;
     //    var key = "";
-       
+
     //    for (var i = 0; i < Locations.length; i++) {
-          
+
     //        key = "loc_" + Locations[i].ID;
-            
+
     //        var newLocation = {
     //            key: key,
     //            type: 'Location',
@@ -24,7 +24,7 @@
     //            geostamp_lat : Locations[i].geostamp_lat,
     //            geostamp_lon : Locations[i].geostamp_lon
     //        };
-            
+
     //        console.log("Saving newlocation with key: " + newLocation.key);
 
     //        //lawnchair_store.save(newLocation);
@@ -36,39 +36,35 @@
     function initLocations() {
         console.log("initializing locations...");
         $.getJSON("/api/LocationApi/", function (data) {
-
-            var wrapper  = { 'items': [] }; //need a key 'items' for the array or else store returns undefined
-
-            for (var i = 0; i < data.length; i++) {
-                var obj = {};
-                $.each(data[i], function (key, value) { //add each property
-                    obj[key] = value;
-                });
-                console.log("new obj: " + JSON.stringify(obj));
-                wrapper.items.push(obj);  //push to item array
-            };
-       
-           store.set("Locations", JSON.stringify(wrapper));
+            mapper("Locations", data);
         });
-
-       // var retWrapper = JSON.parse(store.get("Locations"));
-        //var retWrapper = JSON.parse(localStorage.getItem("Locations"));
-          //  console.log(retWrapper);
-
+        //get sample is in WellLocationViewModel -> locationVM()
     };
 
     function checkStore(str, callBack) {
-        if (store.get(str) == null) {
-            callBack();
-        }
-        else {
-            console.log(str + " already initialized"); //debug
-        }
+        if (store.get(str) == null) { callBack(); }
+        else { console.log(str + " already initialized") } //debug
     };
-   
-    checkStore("Locations", initLocations);
 
+    //abstracted helper function for creating a new entity/object store
+    function mapper(entityName, data) {
+        var wrapper = { 'map': '' }; //need a key for the array/map or else store returns undefined
+        var mapObj = {}; //simplifies returning the object by id instead of looping thru an array
 
+        for (var i = 0; i < data.length; i++) {
+            var obj = {};
+            $.each(data[i], function (key, value) { //add each property
+                obj[key] = value;
+            });
+            console.log("new obj: " + JSON.stringify(obj));
+            mapObj[obj.ID] = obj;
+        };
+        wrapper.map = mapObj;
+        store.set(entityName, JSON.stringify(wrapper)); //store the new entity in localstorage
+    }
+
+    //Main Checks
+    checkStore("Locations", initLocations); //check to see of locations have been created locally
 
 });
 
